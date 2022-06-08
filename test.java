9b39caf31f1c0361c615b019b1ea86fd02468ceb
codeu <kkam7942s2@naver.com>
2022-06-08 23:27:18 +0900

    sample

---
+++ test.java
@@ -0,0 +1,9 @@
+import javax.sound.midi.VoiceStatus;
+
+public class test {
+    public static void main(String[] args){
+        for(int i=0;i<10;i++){
+            System.out.print("i = {}", i);
+        }
+    }
+}
